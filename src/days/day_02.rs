71964c0e550bfe6a4c38fd02a7063323981895ef
Simon LEONARD <git-1001af4@sinux.sh>
2024-08-04 00:17:01 +0200

    feat(day-02): part 2

--- src/days/day_02.rs
+++ src/days/day_02.rs
@@ -9,12 +9,20 @@ use crate::{commands::Runnable, days::Day};
 pub struct Command {
     #[clap(value_name = "FILE")]
     file: PathBuf,
+    #[clap(value_enum, default_value_t=Part::Part1)]
+    part: Part,
+}
+
+#[derive(clap::ValueEnum, Clone, Copy)]
+pub enum Part {
+    Part1,
+    Part2,
 }
 
 impl Runnable for Command {
     fn run(&self) {
         let file = File::open(&self.file).expect("Impossible d'ouvrir le fichier");
-        let result = Day02::new().process(io::BufReader::new(file));
+        let result = Day02::new(self.part).process(io::BufReader::new(file));
         println!("The result is \"{}\".", result);
     }
 }
@@ -22,10 +30,11 @@ impl Runnable for Command {
 struct Day02 {
     re_game: Regex,
     re_draw: Regex,
+    part: Part,
 }
 
 impl Day02 {
-    fn new() -> Self {
+    fn new(part: Part) -> Self {
         Self {
             re_game: Regex::new(pomsky! {
                 let rounds = [' ' ",;" ascii_digit "red" "green" "blue"]+;
@@ -37,18 +46,27 @@ impl Day02 {
                 ^ :count([ascii_digit]+) ' ' :color(color) $
             })
             .unwrap(),
+            part,
         }
     }
 }
 
 impl Day<usize> for Day02 {
     fn process(&self, input: impl io::BufRead) -> usize {
-        input
-            .lines()
-            .filter_map(|line| parse_game(&line.unwrap_or("".into()), &self.re_game, &self.re_draw))
-            .filter(|game| game.is_valid_part1())
-            .map(|game| game.id)
-            .sum()
+        let common = input.lines().filter_map(|line| {
+            parse_game(&line.unwrap_or("".into()), &self.re_game, &self.re_draw)
+        });
+
+        match self.part {
+            Part::Part1 => common
+                .filter(|game| game.is_valid_part1())
+                .map(|game| game.id)
+                .sum(),
+            Part::Part2 => common
+                .map(|game| max_cubes(&game.rounds))
+                .map(|Round { red, green, blue }| red * green * blue)
+                .sum(),
+        }
     }
 }
 
@@ -75,6 +93,15 @@ impl Round {
     fn is_valid_part1(&self) -> bool {
         self.red <= 12 && self.green <= 13 && self.blue <= 14
     }
+}
+
+fn max_cubes(vec: &[Round]) -> Round {
+    vec.iter().fold(Round::default(), |mut acc, round| {
+        acc.red = acc.red.max(round.red);
+        acc.green = acc.green.max(round.green);
+        acc.blue = acc.blue.max(round.blue);
+        acc
+    })
 }
 
 fn parse_game(line: &str, re_game: &Regex, re_draw: &Regex) -> Option<Game> {
@@ -121,12 +148,12 @@ mod tests {
 
     #[test]
     fn regexes_compile() {
-        Day02::new();
+        Day02::new(Part::Part1);
     }
 
     #[test]
     fn regex_game_parse() {
-        let day = Day02::new();
+        let day = Day02::new(Part::Part1);
         let hay = "Game 33: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
         let Some(groups) = day.re_game.captures(hay) else {
             panic!("No game ID found");
@@ -140,7 +167,7 @@ mod tests {
 
     #[test]
     fn regex_draw_parse() {
-        let day = Day02::new();
+        let day = Day02::new(Part::Part1);
         let hay = "3 blue";
         let Some(groups) = day.re_draw.captures(hay) else {
             panic!("Can't parse count and color");
@@ -151,7 +178,7 @@ mod tests {
 
     #[test]
     fn parse_game_simple() {
-        let day = Day02::new();
+        let day = Day02::new(Part::Part1);
         assert_eq!(
             parse_game(
                 "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
@@ -183,7 +210,7 @@ mod tests {
 
     #[test]
     fn parse_round_simple() {
-        let day = Day02::new();
+        let day = Day02::new(Part::Part1);
         assert_eq!(
             parse_round("1 red, 1 green, 1 blue", &day.re_draw),
             Some(Round {
@@ -242,5 +269,17 @@ mod tests {
         for round in rounds_bad {
             assert!(!round.is_valid_part1());
         }
+    }
+
+    #[test]
+    fn part2_works() {
+        let file = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+            .as_bytes();
+        let reader = io::BufReader::new(file);
+        assert_eq!(Day02::new(Part::Part2).process(reader), 2286);
     }
 }
